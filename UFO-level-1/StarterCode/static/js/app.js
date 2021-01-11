8958df97143ae8b992da4125dff6326966ad239c
Brett Williams <brettwilliams@Bretts-MacBook-Pro.local>
2021-01-10 21:10:34 -0800

    created click and utable func

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,13 +1,19 @@
 // from data.js
 var tableData = data;
 
-d3.select("#submit").on("click", filterIt):
-d3.select("form").on("submit",filterIt);
-d3.select("clear").on("click",buildPage(tableData));
+d3.select("#submit").on("click", handleClick):
+d3.select("form").on("submit", handleClick);
+d3.select("clear").on("click",ufo_table(tableData));
 
 
 // This function is triggered when the button is clicked
 function handleClick() {
   console.log("Filtered");
   console.log(d3.event.target);
+
+}
+
+function ufo_table(datastuff) {
+    console.log("Matching sightings table done");
+
 }
