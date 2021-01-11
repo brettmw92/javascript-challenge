ef8aee9f79ea03cf37944173d89a63ebd9439922
Brett Williams <brettwilliams@Bretts-MacBook-Pro.local>
2021-01-10 20:58:38 -0800

    filter clearing and rebuild table

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,4 +1,13 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+d3.select("#submit").on("click", filterIt):
+d3.select("form").on("submit",filterIt);
+d3.select("clear").on("click",buildPage(tableData));
+
+
+// This function is triggered when the button is clicked
+function handleClick() {
+  console.log("Filtered");
+  console.log(d3.event.target);
+}
