fdbb3a4feadfd8f0a6e1481ca7f608b9c78b0a6f
Brett Williams <brettwilliams@Bretts-MacBook-Pro.local>
2021-01-18 20:01:39 -0800

    build ufo data

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,19 +1,28 @@
 // from data.js
 var tableData = data;
+// console.log(data);
 
-d3.select("#submit").on("click", handleClick):
-d3.select("form").on("submit", handleClick);
-d3.select("clear").on("click",ufo_table(tableData));
+var tbody = d3.select("tbody")
+// console.log(tbody);
+
+data.forEach(function(ufos) {
+    console.log(ufos);
+});
+
+// d3.select("#submit").on("click", handleClick);
+// d3.select("form").on("submit", handleClick);
+// d3.select("clear").on("click",ufo_table(tableData));
 
 
-// This function is triggered when the button is clicked
-function handleClick() {
-  console.log("Filtered");
-  console.log(d3.event.target);
+// // // // This function is triggered when the button is clicked
+// // function handleClick() {
+// //   console.log("Filtered");
+// //   console.log(d3.event.target);
+// //   var dateInput = d3.select("#dateInput").node().value;
 
-}
+// // }
 
-function ufo_table(datastuff) {
-    console.log("Matching sightings table done");
+// // function ufo_table(datastuff) {
+// //     console.log("Matching sightings table done");
 
-}
+// // }
